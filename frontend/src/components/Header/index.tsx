9d59e1015d01ebcc9a008de3905c180d29e604f7
Danilo Fernandes <daniloaraujofernandes@gmail.com>
2022-07-16 22:59:42 -0300

    Outros Componentes

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+
+import './style.css'
+import logo from '../../assets/socicon_apple.svg'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-conteiner">
+                <img src={logo} alt=""/>
+                    <h1>DSMeta</h1>
+                    <p> Desenvolvido por
+                        <a href="https://www.instagram.com/danilof.fernandes/">@danilof.fernandes </a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ frontend/src/components/NotificationNutton/index.tsx
@@ -0,0 +1,16 @@
+
+import './style.css'
+
+function NotificationButton() {
+    return (
+        <>
+
+            <div className="dsmeta-red-btn">
+                IMAGEM
+            </div>
+
+        </>
+    )
+}
+
+export default NotificationButton
